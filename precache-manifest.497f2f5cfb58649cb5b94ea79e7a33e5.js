965d3294266837feaed9354960a2e57e8974b4a5
Ahmed Safwat <63418052+Ahmed-safwat-2020@users.noreply.github.com>
2020-07-11 05:39:14 +0200

    .

---
+++ precache-manifest.497f2f5cfb58649cb5b94ea79e7a33e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c04403ad39b0a0f2d120c7aa228d910",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dc33fd6a84a01a2bf5b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "98c5c01925b36ebc43bf",
+    "url": "/static/css/main.00392bdf.chunk.css"
+  },
+  {
+    "revision": "4dc33fd6a84a01a2bf5b",
+    "url": "/static/js/2.d9d87cbe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d9d87cbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98c5c01925b36ebc43bf",
+    "url": "/static/js/main.1e21be1b.chunk.js"
+  },
+  {
+    "revision": "4fb080de24627b03831e",
+    "url": "/static/js/runtime-main.f16ea130.js"
+  }
+]);
